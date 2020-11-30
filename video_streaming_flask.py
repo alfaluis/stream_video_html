c1bb274e1e99fff54d0ff4f6301f4ac8d34151eb
alfaluis <alfa.b14@gmail.com>
2020-11-30 09:34:13 -0300

    Update code to get Name/Floor and folder image save to train service

--- video_streaming_flask.py
+++ video_streaming_flask.py
@@ -1,13 +1,20 @@
 import argparse
 import time
-
-from flask import Flask, render_template, Response
+import json
+import os
+from flask import Flask, render_template, Response, request
 import cv2
 import utils
+import shutil
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 HEIGHT = 240
 WIDTH = 320
+start_take_photo = False
+start_time = 0
+count_img = 0
+folder_name = ''
+root = os.path.join(os.getcwd(), 'data')
 
 
 @app.route('/')
@@ -15,12 +22,31 @@ def index():
     return render_template('index.html', w=WIDTH, h=HEIGHT)
 
 
+def create_folder(path_to_create):
+    if os.path.exists(path_to_create):
+        shutil.rmtree(path_to_create)
+    os.makedirs(path_to_create)
+
+
 def gen():
+    global start_take_photo, count_img, start_time, folder_name
     while True:
         ret, frame = vid.read()
         img = cv2.imencode('.jpg', frame)[1].tobytes()
-        detected_faces = utils.detect_face_stream(endpoint=ENDPOINT, key=KEY, image=img)
 
+        if start_take_photo:
+            create_folder(os.path.join(root, folder_name))
+
+            if time.time() - start_time > 3:
+                start_take_photo = False
+            print('taking pictures')
+            cv2.imwrite(os.path.join(root, folder_name, 'img_{}.jpg'.format(count_img)), frame)
+            count_img += 1
+        else:
+            start_time = time.time()
+            count_img = 0
+
+        detected_faces = utils.detect_face_stream(endpoint=ENDPOINT, key=KEY, image=img)
         print('Image num face detected {}'.format(detected_faces))
         color = (255, 0, 0)
         thickness = 2
@@ -36,6 +62,17 @@ def gen():
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(),  mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/route/to/change_color', methods=['POST'])
+def change_color():
+    global start_take_photo, folder_name
+    start_take_photo = bool(request.form['take_pics'])
+    in_floor = request.form['floor'].replace(' ', '')
+    in_name = request.form['name'].replace(' ', '')
+    folder_name = in_name + in_floor
+    print(start_take_photo, in_floor, in_name, folder_name)
+    return json.dumps({'Status': "OK"})
 
 
 if __name__ == '__main__':
@@ -54,7 +91,7 @@ if __name__ == '__main__':
     vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
     # run app
-    app.run(debug=False)
+    app.run(debug=True)
 
     # release camera
     vid.release()

a9bb4e1ce3b669a403fe98cf85169ebd4d368126
alfaluis <alfa.b14@gmail.com>
2020-11-30 09:32:45 -0300

    Final Javascript code to start taking pictures

--- static/javascript.js
+++ static/javascript.js
@@ -1,21 +1,42 @@
-var cTime = 5;
+var cTime = 3;
 var takePic = 3;
-function myTimer(){
-    setTimeout(myTimer, 1000);
-    if (takePic > 0){
-        if (cTime > 0){
-            document.getElementById("texto").innerHTML = "El sistema comenzara a tomar fotos en: ";
-            document.getElementById("tiempo").innerHTML = cTime + "s.";
-            cTime = cTime - 1;
-        }
-        else {
-            document.getElementById("texto").innerHTML = "Tomando foto... Tiempo restante:";
-            document.getElementById("tiempo").innerHTML = takePic + "s.";;
-            takePic = takePic - 1;
-        }
+var startProcess = false;
+var myVar;
+var startTime = new Date();
+var postMess = true;
+
+function postMessage(){
+    document.getElementById("texto").innerHTML = "Finished";
+    var inFloor = document.getElementById("floor").value;
+    var inName = document.getElementById("name").value;
+    postMess = true;
+    clearInterval(myVar);
+    var url = "/route/to/change_color";
+    var data = {take_pics: true, floor: inFloor, name: inName};
+    $.post(url, data);
+}
+
+function activateCountDown(){
+    if (document.getElementById("name").value.length == 0 & document.getElementById("floor").value.length == 0){
+        alert("Nombre no puede estar vacia");
     }
-    else {
-        document.getElementById("texto").innerHTML = "Procesando...";
-        document.getElementById("tiempo").innerHTML = "";
+    else{
+	    myVar = setInterval(myTimer2, 1000);
+	    startTime = new Date();
+    }
+}
+
+function myTimer2() {
+    var d = new Date();
+    var t = d.toLocaleTimeString();
+    document.getElementById("tiempo").innerHTML = Math.round((d - startTime) / 1000);
+    if (postMess == true) {
+        postMess = false;
+        timeoutID = setTimeout(postMessage, 3000);
+    }
+
+    if (Math.abs((startTime - d) / 1000) >= 3){
+  	    document.getElementById("tiempo").innerHTML = "Taking Images...";
+        startTime = new Date();
     }
 }

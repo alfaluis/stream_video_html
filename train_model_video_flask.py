b27073fb7331064c1ad9af8f88d32de67db15de4
alfaluis <alfa.b14@gmail.com>
2020-12-20 22:03:31 -0300

    Add get request to train model. Add check status in upload and training user

--- train_model_video_flask.py
+++ train_model_video_flask.py
@@ -2,30 +2,23 @@ import argparse
 import time
 import json
 import os
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response, request, jsonify
 import cv2
-import utils
 import shutil
 from train_and_detect import train_person_group
 
-# video_streaming_flask.py -k 298b3b2660164139b5b0be02d2c8c219 -sn face-api-dev
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 HEIGHT = 240
 WIDTH = 320
-GROUP_ID = 'Building_1_id'
-GROUP_NAME = 'Building_1'
+GROUP_ID = 'building-1-id'
+GROUP_NAME = 'building-1'
 start_take_photo = False
+train_status = None
 start_time = 0
 count_img = 0
-folder_name = ''
-folder_path = ''
+folder_name, folder_path = '', ''
 root = os.path.join(os.getcwd(), 'data')
-
-
-@app.route('/')
-def index():
-    return render_template('index.html', w=WIDTH, h=HEIGHT)
 
 
 def create_folder(path_to_create):
@@ -35,7 +28,7 @@ def create_folder(path_to_create):
 
 
 def gen():
-    global start_take_photo, count_img, start_time, folder_name, folder_path
+    global start_take_photo, count_img, start_time, folder_path
     while True:
         ret, frame = vid.read()
         img = cv2.imencode('.jpg', frame)[1].tobytes()
@@ -45,15 +38,14 @@ def gen():
                 folder_path = os.path.join(root, folder_name)
                 create_folder(folder_path)
 
-            if (time.time() - start_time > 3) or (count_img >= 15):
+            if time.time() - start_time >= 3:
                 start_take_photo = False
                 print('Stop Taking photos')
-                train_person_group(key=KEY, endpoint=ENDPOINT, group_id=GROUP_ID, group_name=GROUP_NAME,
-                                   path_folder=folder_path, person_group_name=folder_name)
             else:
                 print('taking pictures')
                 cv2.imwrite(os.path.join(root, folder_name, 'img_{}.jpg'.format(count_img)), frame)
                 count_img += 1
+                time.sleep(0.2)
         else:
             start_time = time.time()
             count_img = 0
@@ -62,20 +54,38 @@ def gen():
                b'Content-Type: image/jpeg\r\n\r\n' + img + b'\r\n')
 
 
+@app.route('/')
+def index():
+    return render_template('index.html', w=WIDTH, h=HEIGHT)
+
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(),  mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-@app.route('/route/to/change_color', methods=['POST'])
+@app.route('/client_info', methods=['POST'])
 def change_color():
     global start_take_photo, folder_name
-    start_take_photo = bool(request.form['take_pics'])
-    in_floor = request.form['floor'].replace(' ', '')
-    in_name = request.form['name'].replace(' ', '')
+    response = request.get_json()
+    start_take_photo = bool(response['take_pics'])
+    in_floor = response['floor'].replace(' ', '')
+    in_name = response['name'].replace(' ', '')
     folder_name = in_name + in_floor
-    print(start_take_photo, in_floor, in_name, folder_name)
-    return json.dumps({'Status': "OK"})
+    while start_take_photo:
+        print(start_take_photo, in_floor, in_name, folder_name)
+        time.sleep(.2)
+    return json.dumps({'UploadProcess': "Success"})
+
+
+@app.route('/training_status', methods=['GET'])
+def testfn():
+    global train_status, start_time
+    start_time = time.time()
+    train_status = train_person_group(key=KEY, endpoint=ENDPOINT, group_id=GROUP_ID, group_name=GROUP_NAME,
+                                      path_folder=folder_path, person_group_name=folder_name)
+    message = {'TrainStatus': train_status}
+    return jsonify(message)  # serialize and use JSON headers
 
 
 if __name__ == '__main__':

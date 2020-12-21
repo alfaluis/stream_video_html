9facfc33ab30ae3fb0c8bbf16371c1b3f5b939b4
alfaluis <alfa.b14@gmail.com>
2020-12-20 22:04:53 -0300

    Add get request to validate training process. Clean code

--- static/javascript.js
+++ static/javascript.js
@@ -1,45 +1,103 @@
 var cTime = 0;
 var takePic = 3;
 var startProcess = false;
-var myVar;
+var intervalPreview;
+var intervalCapture;
 var startTime = new Date();
 var postMess = true;
 
-function postMessage(){
-    document.getElementById("texto").innerHTML = "Finished";
-    var inFloor = document.getElementById("floor").value;
-    var inName = document.getElementById("name").value;
-    postMess = true;
-    clearInterval(myVar);
-    var url = "/route/to/change_color";
-    var data = {take_pics: true, floor: inFloor, name: inName};
-    $.post(url, data);
+// Function to process the response from Post message (sentUserInfoPost)
+function processResponse(responseJson){
+    clearInterval(intervalCapture)
+    console.log(responseJson)
+    document.getElementById("texto").innerHTML = "Fotos Tomadas: " + responseJson.UploadProcess;
+  	document.getElementById("tiempo").innerHTML = "";
+  	if (responseJson.UploadProcess == "Success"){
+  	    alert("Fotos subidas exitosamente. \nSe Procedera con registro de usario. Esto puede tomar un tiempo.")
+  	    return true
+  	} else {
+  	    alert("Error al subir fotos. \nPor favor intente nuevamente.")
+  	    return false
+  	}
 }
 
+// Function to start training process and return response from server (flask)
+function trainingProcess(uploadStatus){
+    if (uploadStatus){
+        console.log('upload Status Succeed:');
+        console.log(uploadStatus);
+        document.getElementById("texto").innerHTML = "Training...";
+        fetch('/training_status', {method: "GET"})
+            .then(function(response) {return response.json();})
+            .then(function (text) {
+                console.log('GET response:');
+                console.log(text);
+                cleanInfo()
+                alert("Usuario registrado exitosamente!");})
+            .catch(err => console.log(err));
+    } else {
+        cleanInfo()
+        console.log('upload Status Fail');
+        console.log(uploadStatus);
+    }
+}
+
+// Post user information to server (flask)
+function sentUserInfoPost(){
+    // Disable intervalPeview Interval and activate captureCountDown
+    clearInterval(intervalPreview);
+    intervalCapture = setInterval(captureCountDown, 1000);
+    
+    // get values from Form
+    var inFloor = document.getElementById("floor").value;
+    var inName = document.getElementById("name").value;
+    
+    // restart variable
+    startTime = new Date();
+    // dictionary to sent
+    var infoData = {take_pics: true, floor: inFloor, name: inName};
+    
+    // post message with 
+    fetch('/client_info', {method: "POST",
+                                     body:  JSON.stringify(infoData),
+                                     headers: {"Content-type": "application/json"}}
+        )
+        .then(function(response) {return response.json();})
+        .then(processResponse)
+        .then(trainingProcess)
+        .catch(err => console.log(err));
+}
+
+// Function called once the client press start button
 function activateCountDown(){
+    // validate information
     if (document.getElementById("name").value.length == 0 | document.getElementById("floor").value.length == 0){
-        alert("Nombre no puede estar vacia");
+        alert("Nombre o piso no puede estar vacia");
     }
     else{
-	    myVar = setInterval(myTimer2, 1000);
+        // activate timer function every 1s
+	    intervalPreview = setInterval(previewCountDown, 1000);
+	    setTimeout(sentUserInfoPost, 3000);
+	    document.getElementById("btnStart").disabled = true;
 	    startTime = new Date();
     }
 }
 
-function myTimer2() {
+function previewCountDown() {
     var d = new Date();
-    var t = d.toLocaleTimeString();
     document.getElementById("texto").innerHTML = "El Proceso comenzara a tomar fotos en: ";
     document.getElementById("tiempo").innerHTML = Math.round(3 - (d - startTime) / 1000);
-    if (postMess == true) {
-        postMess = false;
-        timeoutID = setTimeout(postMessage, 3000);
-    }
+}
 
-    if (Math.abs((startTime - d) / 1000) >= 3){
-  	    document.getElementById("texto").innerHTML = "Tomando fotos... Tiempo restamte:";
-  	    document.getElementById("tiempo").innerHTML = Math.round(3 - (d - startTime) / 1000);
-        startTime = new Date();
-    }
+function captureCountDown(){
+    var d = new Date();
+    document.getElementById("texto").innerHTML = "Tomando fotos... Tiempo restante:";
+  	document.getElementById("tiempo").innerHTML = Math.round(3 - (d - startTime) / 1000);
+}
+
+function cleanInfo(){
+    document.getElementById("btnStart").disabled = false;
+    document.getElementById("texto").innerHTML = "";
+    document.getElementById("formUser").reset();
 }
 

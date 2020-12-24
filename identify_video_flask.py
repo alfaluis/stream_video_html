7b0f18f2905b48b5db413c25f7ca4b53de9b1f1a
alfaluis <alfa.b14@gmail.com>
2020-12-23 22:07:54 -0300

    Clean code and add get method

--- identify_video_flask.py
+++ identify_video_flask.py
@@ -1,27 +1,27 @@
 import argparse
 import time
 
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, jsonify
 import cv2
 import utils
 from train_and_detect import identify_from_video
+
+# identify_video_flask.py -k 298b3b2660164139b5b0be02d2c8c219 -sn face-api-dev
 
 app = Flask(__name__)
 HEIGHT = 240
 WIDTH = 320
 GROUP_ID = 'building-1-id'
 GROUP_NAME = 'building-1'
-
-
-@app.route('/')
-def index():
-    return render_template('index.html', w=WIDTH, h=HEIGHT)
+person_name = 'None Detected'
+flag = False
 
 
 def gen():
+    global person_name
     while True:
         ret, frame = vid.read()
-        frame = identify_from_video(endpoint=ENDPOINT, key=KEY, group_id=GROUP_ID, frame=frame)
+        frame, person_name = identify_from_video(endpoint=ENDPOINT, key=KEY, group_id=GROUP_ID, frame=frame)
         img_send = cv2.imencode('.jpg', frame)[1].tobytes()
 
         time.sleep(1)
@@ -29,9 +29,21 @@ def gen():
                b'Content-Type: image/jpeg\r\n\r\n' + img_send + b'\r\n')
 
 
+@app.route('/')
+def index():
+    global person_name
+    return render_template('index_identify.html', w=WIDTH, h=HEIGHT, name=person_name)
+
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(),  mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/detection', methods=['GET'])
+def testfn():
+    message = {'PersonName': 'Luis'}
+    return jsonify(message)  # serialize and use JSON headers
 
 
 if __name__ == '__main__':

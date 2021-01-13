809125a07aaf738f6b2be9b2c16955eef0823caa
alfaluis <alfa.b14@gmail.com>
2021-01-13 17:06:26 -0300

    additional function used to process and deal with information. This functions are used for identify_from_video and train_model_video_flask

--- train_and_detect.py
+++ train_and_detect.py
@@ -9,14 +9,30 @@ import utils
 
 
 def train_person_group(key, endpoint, group_id, group_name, path_folder, person_group_name):
+    """Function to add a new user to the Azure Service Identification.
 
+    @param key: key connection of Azure Service
+    @type key: str
+    @param endpoint: Endpoint Azure Service
+    @type endpoint: str
+    @param group_id: Group ID of the identify service
+    @type group_id: str
+    @param group_name: Group Name of the identify service
+    @type group_name: str
+    @param path_folder: Folder path with the images of the user to add
+    @type path_folder: str
+    @param person_group_name: Name used to store the user in the Service
+    @type person_group_name: str
+    @return: Return train success or fail
+    @rtype: str
+    """
     face_client = FaceClient(endpoint, CognitiveServicesCredentials(key))
 
     print('Person group:', group_name)
     try:
         # create a PersonGroup reference class
         face_client.person_group.create(person_group_id=group_id, name=group_name, recognition_model="recognition_03")
-    except APIErrorException as err:
+    except APIErrorException:
         print('person group {0}:{1} already exist.'.format(group_id, group_name))
 
     # create Person object inside the person group
@@ -41,50 +57,132 @@ def train_person_group(key, endpoint, group_id, group_name, path_folder, person_
         elif training_status.status is TrainingStatusType.failed:
             print('Training the person group has failed.')
         time.sleep(5)
+    return training_status.status
 
 
-def identify_from_video(endpoint, key, group_id, frame):
+def process_response(response):
+    """This function try to clean the response got from the Azure Face Service.
+       There are few cases to cover to receive a clean answer:
+            - Response type list: If list is empty then no face/person detected/identified, else we return the list
+            - Response type dict: If dict contains key 'name' then it is returning person_info,
+                else the it's returning an error. If error code is 429 we exceed the free tier otherwise there is a
+                connection error message
+
+    @param response: Object with the response receive from Azure Service
+    @type response: Dict or List
+    @return: response object or None
+    @rtype: (Dict, List or None,
+    """
+    if isinstance(response, list):
+        if len(response) == 0:
+            return None, 0
+        else:
+            return response, 0
+    elif isinstance(response, dict):
+        try:
+            response['name']
+            return response, 0
+        except KeyError:
+            print('error handling')
+            err_code = response['error']['code']
+            if int(err_code) == 429:
+                return None, 10
+            else:
+                raise Exception(response['error'])
+    else:
+        return None, 0
+
+
+def detect_face(endpoint, key, frame):
+    """Run request to Azure Face service and return faces ID detected
+
+    @param endpoint: Endpoint Azure Service
+    @type endpoint: str
+    @param key:  key connection of Azure Service
+    @type key: str
+    @param frame: Frame Image
+    @type frame: numpy.ndarray
+    @return: (List with all faceIds and rectangles of the detected faces, sleep seconds)
+    @rtype: (list or None, int)
+    """
     img = cv2.imencode('.jpg', frame)[1].tobytes()
     attributes = ''
     detected_faces = utils.detect_face_stream(endpoint=endpoint, key=key, image=img, face_attributes=attributes,
                                               recognition_model='recognition_03')
 
-    print('detected faces:', type(detected_faces))
-    print(detected_faces)
-    # ADD CONDITIONAL, IF DETECTED IS EMPTY END FUNCTION
-    if len(detected_faces) == 0:
-        return frame
-    if isinstance(detected_faces, dict):
-        try:
-            err_code = detected_faces['error']['code']
-            if int(err_code) == 429:
-                time.sleep(10)
-        except TypeError:
-            print('error in dict keys, please check: error and code values')
-        except ValueError:
-            print('Unable to convert error code to integer')
-        finally:
-            return frame
+    detected_faces, sleep_time = process_response(detected_faces)
+    return detected_faces, sleep_time
+
+
+def identify_and_process(detected_faces, endpoint, key, group_id):
+    identify_response = list()
+
+    # Face Ids to search in the person group (10 max)
     faces_ids = [f['faceId'] for f in detected_faces]
     identify_output = utils.identify_faces(endpoint=endpoint, key=key, group_id=group_id, face_id_list=faces_ids)
+    identify_output, sleep_time = process_response(identify_output)
+    if sleep_time == 10:
+        time.sleep(sleep_time)
+
+    if identify_output is not None:
+        for person in identify_output:
+            # print(person)
+            try:
+                # get the most confidence candidate to process
+                candidate = person['candidates'][0]
+                # while cycle to ensure we return person info (free tier limit)
+                person_info = None
+                while person_info is None:
+                    person_info = utils.get_person_info(endpoint, key, group_id, candidate['personId'])
+                    person_info, sleep_time = process_response(person_info)
+                    if sleep_time == 10:
+                        time.sleep(sleep_time)
+
+                identify_response.append({'faceId': person['faceId'],
+                                          'name': person_info['name'],
+                                          'confidence': candidate['confidence']})
+            except IndexError:
+                # No candidates found
+                identify_response.append({'faceId': person['faceId'],
+                                          'name': None,
+                                          'confidence': 0})
+        return identify_response
+    return None
+
+
+def identify_from_video(endpoint, key, group_id, frame):
+    """ Main function to detect faces in 'frame' and detect if its are contained in the 'group_id'
+
+    @param endpoint: Endpoint Azure Service
+    @type endpoint: str
+    @param key: key connection of Azure Service
+    @type key: str
+    @param group_id:
+    @type group_id: str
+    @param frame: Frame Image
+    @type frame: numpy.ndarray
+    @return: (frame image, string name)
+    @rtype: (numpy.ndarray, str or None)
+    """
+    start_measure = time.time()
     thickness = 2
-    for person in identify_output:
-        print(type(person['faceId']))
-        print('Result of face: {0}.'.format(person['faceId']))
-        face = [face for face in detected_faces if face['faceId'] == person['faceId']][0]
-        if not len(person['candidates']) == 0:
-            candidate = person['candidates'][0]
-            print('Identified in {} with a confidence: {}.'.format(person['faceId'], candidate['confidence']))
-            person_info = utils.get_person_info(endpoint, key, group_id, candidate['personId'])
-            print('Person INFO:', person_info)
-            print('Name Group person identified: {0}'.format(person_info['name']))
-            color = (0, 255, 0)
+    detected_faces, sleep_time = detect_face(endpoint, key, frame)
+    print(detected_faces)
+    if sleep_time == 10:
+        time.sleep(sleep_time)
+
+    if detected_faces is not None:
+        faces_info = identify_and_process(detected_faces, endpoint, key, group_id)
+        print('Detected: {} and Info {}'.format(detected_faces, faces_info))
+        for face, info in zip(detected_faces, faces_info):
+            if info['confidence'] > 0.5:
+                color = (0, 255, 0)
+            else:
+                color = (0, 0, 255)
             frame = cv2.rectangle(frame, *utils.get_rectangle(face), color, thickness)
-            x, y = face['faceRectangle']['left'], face['faceRectangle']['top'] - 5
-            cv2.putText(frame, person_info['name'], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 1)
-        else:
-            print('no match found')
-            color = (255, 0, 0)
-            frame = cv2.rectangle(frame, *utils.get_rectangle(face), color, thickness)
-    
-    return frame
+
+        print('Total time required:', time.time() - start_measure)
+        return frame, faces_info
+
+    print('Total time required:', time.time() - start_measure)
+    return frame, None
